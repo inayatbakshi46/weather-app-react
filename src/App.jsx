73735d10cf72e11f321a5d0ba4ba8064caa46382
inayatbakshi46 <inayatbakshi75@gmail.com>
2024-01-31 16:37:29 +0000

    Add geo location

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,61 @@ import Footer from './components/Footer';
 
 const App = () => {
   const time = new Date().getHours();
-  const storedCity = localStorage.getItem("city");
+  
   const [data, setData] = useState(null);
-  const [city, setCity] = useState(storedCity || "srinagar");
+  const [city, setCity] = useState("");
 
   const key = import.meta.env.VITE_KEY;
 
   useEffect(() => {
-    const fetchData = async () => {
+    const coordsData = async (lat, long) => {
       try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`);
-        const result = await response.json();
-        setData(result);
+         const val = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`);
+         const res = await val.json();
+         setData(res)
       } catch (error) {
-        console.log(error);
+         console.log(error);
       }
-    };
-    fetchData();
-  }, [city, key]);
+      
+    
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+          coordsData(latitude, longitude);
+        },
+        error => {
+          console.log(`Error getting location: ${error.message}`);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, [])
+
+  useEffect(() => {
+    if (city !== "") { // Check if the city is not an empty string
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`);
+          const result = await response.json();
+          setData(result);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchData();
+    }
+  }, [city]);
+
 
   const changeCity = (val) => {
     setData(null);
-    localStorage.setItem("city", val);
-    setCity(val); // Update state directly with the new value
+    
+    setCity(val);
   };
 
   return (
