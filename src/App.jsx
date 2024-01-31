8700a445fbe2ac54cf5545df8aee6d651590d842
inayatbakshi46 <inayatbakshi75@gmail.com>
2024-01-31 14:36:53 +0000

    Add local storage to store users city

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,50 @@
 import React, { useState, useEffect } from 'react';
-
 import Search from './components/Search';
-
 import Weather from './components/Weather';
-
 import Loading from './components/Loading';
-
 import Invalid from './components/Invalid';
-
 import Footer from './components/Footer';
 
-
 const App = () => {
-  const time = new Date().getHours()
+  const time = new Date().getHours();
+  const storedCity = localStorage.getItem("city");
   const [data, setData] = useState(null);
-  const [city, setCity] = useState("srinagar");
-  
+  const [city, setCity] = useState(storedCity || "srinagar");
+
   const key = import.meta.env.VITE_KEY;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`);
-        const result = await response.json()
+        const result = await response.json();
         setData(result);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
-  }, [city]);
+  }, [city, key]);
 
   const changeCity = (val) => {
-    setData(null)
-    setCity(val)
-  }
+    setData(null);
+    localStorage.setItem("city", val);
+    setCity(val); // Update state directly with the new value
+  };
+
   return (
     <main className={time > 18 ? 'dark' : ''}>
-      
       <div className=" bg-gradient-to-t from-day dark:bg-gradient-to-t dark:from-night dark:to-black dark:text-dark font-custom">
-      <section className="">
-      
-      <Search changeCity={changeCity} />
-        
-      </section>
-      <section>
-        
-        {data ? (data.cod == 200 ? <Weather data={data} /> : <Invalid />) : <Loading />}
-        
-      </section>
         <section>
-        <Footer />
+          <Search changeCity={changeCity} />
+        </section>
+        <section>
+          {data ? (data.cod === 200 ? <Weather data={data} /> : <Invalid />) : <Loading />}
+        </section>
+        <section>
+          <Footer />
         </section>
       </div>
-      
     </main>
   );
 };
