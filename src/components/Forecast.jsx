1630ad425ee7a59becfd26fad494ef083266aed5
inayatbakshi46 <inayatbakshi75@gmail.com>
2024-02-01 11:24:22 +0000

    Fix forecast issue

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,14 +2,14 @@ import React, {useEffect, useState} from 'react';
 import Loading from './Loading';
 
 const Forecast = (props) => {
-  const [data, setData] = useState(null);
+  const [val, setVal] = useState(null);
 
   useEffect(() => {
     const coordsData = async (lat, long) => {
       try {
          const val = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&cnt=4&appid=${props.key}&units=metric`);
          const res = await val.json();
-         setData(res.list)
+         setVal(res.list)
       } catch (error) {
          console.log(error);
       }
@@ -39,7 +39,7 @@ const Forecast = (props) => {
         try {
           const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&cnt=4&appid=${props.api}&units=metric`);
           const result = await response.json();
-          setData(result.list);
+          setVal(result.list);
         } catch (error) {
           console.error(error);
         }
@@ -53,22 +53,24 @@ const Forecast = (props) => {
     const time = new Date(0, 0, 0, hours, minutes);
     return time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
   };
-  return (
+return (
+<div>
+  {val ? (
     <div className="p-2 flex flex-col justify-between">
       <h1 className="p-4 border-b-2 dark:border-dark border-black text-xl font-bold tracking-wider">Hourly</h1>
-      {data ? (data.map((item, index) => {
-      return (
-        <div key={index}  className="flex items-center justify-between p-4">
-        <p>{convertTo12HourFormat(item.dt_txt.split(' ')[1])}</p>
+      {val.map((item, index) => (
+        <div key={index} className="flex items-center justify-between p-4">
+          <p>{convertTo12HourFormat(item.dt_txt.split(' ')[1])}</p>
           <p>{item.weather[0].description}</p>
           <p>{item.main.humidity}%</p>
           <p>{item.main.temp} °C</p>
-          </div>
-      );
-
-      })) : <Loading />}
+        </div>
+      ))}
     </div>
-  );
+  ) : null}
+</div>
+);
+
 }
 
 export default Forecast;
